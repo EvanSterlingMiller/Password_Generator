59c40064784441c95872e00de659ca99d0f65561
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-02-13 18:06:41 -0500

    trying again

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-=======
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -21,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 var number_chars = ["0", "1", "2", "3", "4", "5","6","7","8","9"];
 var lowercase_chars = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercars_chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",'P',"Q","R","S","T","U","V","W","X","Y","Z"];
-var special_chars = ["'","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'",'"',"<",".",">","/","?"];
+var special_chars = ["'","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","|",";",":","'",'"',"<",".",">","/","?"];
 
 // setting the variables for password and chars 
 var password = '';
@@ -102,4 +86,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
->>>>>>> adf2390 (major changes)
